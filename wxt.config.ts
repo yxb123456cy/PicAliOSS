c0147dae2b29ecb2b1f4c433a316e1f93b904b2a
yxb123456cy <2186256471@qq.com>
2026-04-09 13:49:25 +0800

    feat(manifest): 更新manifest配置

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,4 +3,19 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-vue'],
+  // 插件manifestV3配置
+  manifest: {
+    // 插件名称
+    name: 'PicAliOSS',
+    // 插件作者email联系方式
+    author: {
+      email: '2186256471@qq.com',
+    },
+    // 插件版本号
+    version: '1.0',
+    // 插件版本名称
+    version_name: '1.0-beta',
+    // 插件描述
+    description: '🚀 轻量级阿里云OSS专用图床浏览器插件 | 专注极简、极速、极稳'
+  },
 });
